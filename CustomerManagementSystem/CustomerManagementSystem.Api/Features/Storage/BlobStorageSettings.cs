7c110a75627af1ba2e53e38307565308fd9cd354
a98c14 <selimyesilkaya@gmail.com>
2021-08-08 02:44:18 +0300

    remove unused files

--- CustomerManagementSystem/CustomerManagementSystem.Api/Features/Storage/BlobStorageSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace CustomerManagementSystem.Core.Storage
-{
-  public class BlobStorageSettings
-  {
-    public string ContainerName { get; set; }
-    public string ConnectionString { get; set; }
-  }
-}
