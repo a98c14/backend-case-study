7b5d0932a5f53aa30eb35cf7b8d6032a247bba48
a98c14 <selimyesilkaya@gmail.com>
2021-08-08 15:53:52 +0300

    add email validation service

---
+++ CustomerManagementSystem/CustomerManagementSystem.Services/EmailValidationService.cs
@@ -0,0 +1,18 @@
+﻿using CustomerManagementSystem.Services.Interfaces;
+
+namespace CustomerManagementSystem.Services
+{
+    public class EmailValidationService : IEmailValidationService
+    {
+        public long GenerateToken()
+        {
+            // TODO(selim): Generate a token 
+            return 0;
+        }
+
+        public void SendOTPMail(string mail, string token)
+        {
+            // TODO(selim): Send mail to user with given token
+        }
+    }
+}
---
+++ CustomerManagementSystem/CustomerManagementSystem.Services/Interfaces/IEmailValidationService.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomerManagementSystem.Services.Interfaces
+{
+    public interface IEmailValidationService
+    {
+        public long GenerateToken();
+        public void SendOTPMail(string mail, string token);
+    }
+}
